4f3a8b89354be65f979ad27b7549f8d52d29992c
Tran Hoang <hoangtran.working@gmail.com>
2023-05-13 15:04:35 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by sakai on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
